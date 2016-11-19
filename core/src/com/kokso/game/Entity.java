bffcdfb8126a800da28ff44747294b125b4fd08f
SKmtt <matej.kaduk@student.tuke.sk>
2016-11-19 14:17:06 +0100

    provisorische animation

--- core/src/com/kokso/game/Entity.java
+++ core/src/com/kokso/game/Entity.java
@@ -7,11 +7,20 @@ public class Entity{
     private Texture image;
     private int height;
     private int width;
+    private float x, y;
 
     public Entity(Texture image){
         this.image = image;
         this.height = image.getHeight();
         this.width = image.getWidth();
+    }
+
+    public Entity(Texture image, float x, float y){
+        this.image = image;
+        this.height = image.getHeight();
+        this.width = image.getWidth();
+        this.x = x;
+        this.y = y;
     }
 
     public void dispose(){
@@ -28,5 +37,21 @@ public class Entity{
 
     public int height(){
         return this.height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
     }
 }
--- core/src/com/kokso/game/Game.java
+++ core/src/com/kokso/game/Game.java
@@ -10,7 +10,13 @@ import java.awt.*;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Entity testLayer;
+	Entity background;
+	Entity clouds;
+	Entity castle;
+	Entity trees;
+	Entity bigAssTrees;
+	Entity road;
+	Entity tower;
 	Dimension screensize;
 	int screenHeight, screenWidth; int i = 0;
 
@@ -23,22 +29,42 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		testLayer = new Entity(new Texture("assets/badlogic.jpg"));
+		background = new Entity(new Texture("assets/test/1.png"));
+		clouds = new Entity(new Texture("assets/test/2.png"), 50, 0);
+		tower = new Entity(new Texture("assets/test/7.png"), 0, 0);
+		castle = new Entity(new Texture("assets/test/6.png"), 0, 0);
+		road = new Entity(new Texture("assets/test/5.png"), 0, 0);
+		trees = new Entity(new Texture("assets/test/4.png"), 0, 0);
+		bigAssTrees = new Entity(new Texture("assets/test/3.png"), 0, 0);
 
 	}
 
 	@Override
 	public void render () {
+		this.update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(testLayer.getImage(), screenWidth-testLayer.width()-i++, screenHeight - testLayer.height());
+		batch.draw(background.getImage(), 690,180);
+		batch.draw(clouds.getImage(), 690+clouds.getX(),180);
+		batch.draw(tower.getImage(), 690,180);
+		batch.draw(castle.getImage(), 690,180);
+		batch.draw(trees.getImage(), 690,180);
+		batch.draw(road.getImage(), 690,180);
+		batch.draw(bigAssTrees.getImage(), 690-bigAssTrees.getX(),180-bigAssTrees.getY(), bigAssTrees.width()+ (bigAssTrees.getX()*2), bigAssTrees.height()+(bigAssTrees.getY()*2) );
 		batch.end();
 	}
-	
+
+
+	private void update(){
+		clouds.setX(clouds.getX()-0.2f);
+		bigAssTrees.setX(bigAssTrees.getX()+1);
+		bigAssTrees.setY(bigAssTrees.getY()+1f);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		testLayer.dispose();
+		background.dispose();
 	}
 }
